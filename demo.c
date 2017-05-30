f33e653ccc258dac83b2909edf11536fea6196e9
sebgiles <mrsebgiles@gmail.com>
2017-05-30 15:04:10 +0200

    random is default

--- demo.c
+++ demo.c
@@ -17,19 +17,19 @@ int main(int argc, char *argv[]) {
     max = atoi(argv[2]);
   else
     max = RAND_MAX;
-  //srand(time(NULL));
-  //srand(0);
 	int n = atoi(argv[1]);
   int* v = malloc(n*sizeof(int));
+
+  srand(time(NULL));
   randomize(v, n, max);
-  print(v, n);
+
 	indextree t = newindextree(v,n);
 	int x;
 	int* f;
 	while(1){
     printf("What are you looking for? ");
 		scanf("%d", &x);
-		if (x==-1) break;
+		if (x == -1) break;
 		f = sorch(t, x);
 		if(f)
 			printf("Found \"%d\" at %x\n",*f, (int)f);
